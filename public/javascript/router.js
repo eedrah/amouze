374363ac18e0d8033506da8c98f79daefeb1db72
Eedrah <eedrah@users.noreply.github.com>
2015-03-09 21:28:08 +1300

    display 'scroll here to hunt'

--- public/javascript/router.js
+++ public/javascript/router.js
@@ -2,7 +2,15 @@ $(startListening);
 
 function startListening(){
 	$('#container').one('mouseover', function(){
+
     	new catController();
         (new mouseController).startPlacingMice();
+        removeInstructions();
 	});
+};
+
+function removeInstructions(){
+    $('#instructions').fadeOut(function(){
+        $(this).remove();
+    });
 };
