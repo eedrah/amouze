23c309e489b3b41f73e9d8897f1b8a95065e2874
Eedrah <eedrah@users.noreply.github.com>
2015-03-09 22:42:15 +1300

    increment time

--- public/javascript/router.js
+++ public/javascript/router.js
@@ -2,7 +2,7 @@ $(startListening);
 
 function startListening(){
 	$('#container').one('mouseover', function(){
-
+        startTimer();
     	new catController();
         (new mouseController).startPlacingMice();
         removeInstructions();
@@ -13,4 +13,14 @@ function removeInstructions(){
     $('#instructions').fadeOut(function(){
         $(this).remove();
     });
+};
+
+function startTimer(){
+    var interval = 30;
+    var intervalSeconds = interval / 1000;
+    var time = 0;
+    setInterval(function(){
+        time += intervalSeconds;
+        $('#totalTime').text(time.toFixed(2));
+    }, interval);
 };
